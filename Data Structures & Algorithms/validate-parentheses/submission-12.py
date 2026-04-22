7f82299071d5588eaaf8e1a85221001d3d958eb4
levonl22 <levon.lau22@gmail.com>
2026-04-22 12:56:26 -0400

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {'}':'{', ']':'[', ')': '('}
+        stack = []
+
+        for c in s:
+            if c in brackets:
+                if stack and stack[-1] == brackets[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return not stack
