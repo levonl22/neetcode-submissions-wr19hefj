716e94ca59beaf51d7fe22c7f1ea8f2363267a1f
Levon Lau <levon.lau22@gmail.com>
2026-05-23 15:54:10 -0400

    Update: binary-tree-diameter - submission-9

--- Data Structures & Algorithms/binary-tree-diameter/submission-9.py
+++ Data Structures & Algorithms/binary-tree-diameter/submission-9.py
@@ -15,9 +15,10 @@ class Solution:
             
             left = dfs(root.left)
             right = dfs(root.right)
+
             self.res = max(self.res, left + right)
-            
-            return (1 + max(left, right))
+
+            return 1 + max(left, right)
         
         dfs(root)
         return self.res
